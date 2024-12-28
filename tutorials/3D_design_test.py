aeabf91a0d0548023372771bb913a07651528b2c
Martin Beaudoin <4019759+mbeaudoin@users.noreply.github.com>
2024-12-27 21:49:58 -0500

    Tutorial : 3D_design_test.py Adjust capitalization for class Airfoil3D and Airfoil2D

--- tutorials/3D_design_test.py
+++ tutorials/3D_design_test.py
@@ -1,14 +1,14 @@
 import sys
 sys.path.insert(0,'../')
 import numpy as np
-from pyturbo.aero import airfoil2D
+from pyturbo.aero import Airfoil2D
 from pyturbo.helper import exp_ratio
 
-stator_hub = airfoil2D(alpha1=0,alpha2=72,axial_chord=0.038,stagger=58) # This creates the camberline
+stator_hub = Airfoil2D(alpha1=0,alpha2=72,axial_chord=0.038,stagger=58) # This creates the camberline
 # Building Leading Edge
 stator_hub.le_thickness_add(0.04)
-# Building the Pressure side 
-ps_height = [0.0500,0.0200,-0.0100] # These are thicknesses 
+# Building the Pressure side
+ps_height = [0.0500,0.0200,-0.0100] # These are thicknesses
 stator_hub.ps_thickness_add(thicknessArray=ps_height,expansion_ratio=1.2)
 
 ss_height=[0.2400, 0.2000, 0.1600, 0.1400]
@@ -18,10 +18,10 @@ stator_hub.te_create(radius=0.001,wedge_ss=2.5,wedge_ps=2.4)
 
 stator_hub.flow_guidance2(10)
 # stator_hub.plot2D()
-stator_mid = airfoil2D(alpha1=10,alpha2=72,axial_chord=0.038,stagger=45) # This creates the camberline
+stator_mid = Airfoil2D(alpha1=10,alpha2=72,axial_chord=0.038,stagger=45) # This creates the camberline
 # Building Leading Edge
 stator_mid.le_thickness_add(0.06)
-# Building the Pressure side 
+# Building the Pressure side
 ps_height = [0.0900,0.0500,0.0200] # These are thicknesses normalized by the chord
 stator_mid.ps_thickness_add(thicknessArray=ps_height,expansion_ratio=1.2)
 
@@ -32,10 +32,10 @@ stator_mid.te_create(radius=0.0012,wedge_ss=3.5,wedge_ps=2.4)
 
 stator_mid.flow_guidance2(10)
 
-stator_tip = airfoil2D(alpha1=5,alpha2=60,axial_chord=0.038,stagger=40) # This creates the camberline
+stator_tip = Airfoil2D(alpha1=5,alpha2=60,axial_chord=0.038,stagger=40) # This creates the camberline
 # Building Leading Edge
 stator_tip.le_thickness_add(0.04)
-# Building the Pressure side 
+# Building the Pressure side
 ps_height = [0.0900,0.0500,0.0200] # These are thicknesses normalized by the chord
 stator_tip.ps_thickness_add(thicknessArray=ps_height,expansion_ratio=1.2)
 
@@ -47,13 +47,14 @@ stator_tip.te_create(radius=0.001,wedge_ss=1.5,wedge_ps=2.4)
 stator_tip.flow_guidance2(5)
 
 
-from pyturbo.aero import airfoil3D, stack_type
+from pyturbo.aero import Airfoil3D, stack_type
 
-stator3D = airfoil3D(profileArray=[stator_hub,stator_mid,stator_tip],profile_loc=[0.0,0.5,1.0], height = 0.04)
+stator3D = Airfoil3D(profileArray=[stator_hub,stator_mid,stator_tip],profile_loc=[0.0,0.5,1.0], height = 0.04)
 stator3D.stack(stack_type.centroid) # Stators are typically stacked with leading edge; rotors with centroid or trailing edge
-stator3D.sweep(sweep_y=[0,-0.05,0.05], sweep_z=[0.0, 0.5, 1]) # Z =1 is blade tip, Z = 0 is blade hub. The units are in percentage 
+stator3D.sweep(sweep_y=[0,-0.05,0.05], sweep_z=[0.0, 0.5, 1]) # Z =1 is blade tip, Z = 0 is blade hub. The units are in percentage
 stator3D.lean(leanX=[0,0.01,-0.02],leanZ=[0,0.5,1])
-stator3D.create_blade(nProfiles=20,profile_points=160,trailing_edge_points=20)
+stator3D.create_blade(nProfiles=20,num_points=160,trailing_edge_points=20)
 # stator3D.plot3D()
 print('check')
 stator3D.export_solidworks('rotor')
+stator3D.export_stl('rotor_demo')
